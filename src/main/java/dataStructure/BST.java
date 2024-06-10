04e3b644671c05ab325f09f9bc8138c700b593ca
Joao <pel118201@gmail.com>
2024-06-10 15:52:27 -0300

    Método insert

--- src/main/java/dataStructure/BST.java
+++ src/main/java/dataStructure/BST.java
@@ -57,9 +57,13 @@ public class BST {
 
     public static void main(String[] args) {
         BST tree = new BST();
-        tree.insert(2);
-        tree.insert(1);
-        tree.insert(3);
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
+        tree.insert(8);
+        tree.insert(17);
+        tree.insert(42);
+        tree.insert(72);
 
         tree.inOrder();
 
