84fc3598958d70955cfdb68760a91d95487abbf8
Joao <pel118201@gmail.com>
2024-06-10 12:58:07 -0300

    Método GetFirst

--- src/main/java/dataStructure/Queue.java
+++ src/main/java/dataStructure/Queue.java
@@ -1,4 +1,33 @@
 package dataStructure;
 
 public class Queue {
+
+    private Node first;
+    private Node last;
+    private int lenght;
+
+    public class Node{
+        int value;
+        Node next;
+// Construtores
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public Queue(int value) {
+        Node newNode = new Node(value);
+        first = newNode;
+        last = newNode;
+        lenght++;
+    }
+
+ // Métodos
+    public void getFirst(){
+        if(first != null){
+            System.out.println(" First: " + first.value);
+        }else{
+            System.out.println("A fila está vazia");
+        }
+    }
 }
