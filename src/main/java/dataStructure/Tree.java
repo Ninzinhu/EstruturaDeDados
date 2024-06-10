b5c9293889bf7be629c8fe1849b80f72bccb44e6
Joao <pel118201@gmail.com>
2024-06-10 15:04:43 -0300

    Método de PreOrder

--- src/main/java/dataStructure/Tree.java
+++ src/main/java/dataStructure/Tree.java
@@ -27,7 +27,7 @@ public class Tree {
     public void insert(int value) {
         if (root == null) root = new Node(value); // Minha Arvores está vazia e agora possui um novo elemento
         else {
-            Node newNode = new Node(value)
+            Node newNode = new Node(value);
             Queue<Node> queue = new LinkedList<>();
             queue.add(root);
             while (queue.size() > 0) { // Laço de controle ( Enquanto minha fila tiver elementos)
@@ -49,15 +49,38 @@ public class Tree {
         }
     }
 
+
+    // Método preOrder
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(final Node node) {
+        // R E D (Raiz,esquerda e direita)
+        if (node == null) return;
+        System.out.println(node.value);
+        preOrder(node.left);
+        preOrder(node.right);
+
+    }
+
+
     public static void main(String[] args) {
         Tree tree = new Tree();
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
+        tree.insert(37);
+        tree.insert(11);
+        tree.insert(66);
+        tree.insert(8);
+        tree.insert(17);
+        tree.insert(42);
+        tree.insert(72);
 
-        System.out.println(tree.root);
-        System.out.println(tree.root.left);
-        System.out.println(tree.root.right);
+        System.out.println(tree.root.value);
+        System.out.println(tree.root.left.value);
+        System.out.println(tree.root.right.value);
+
+        System.out.println(tree.root.isLeaf());
+        System.out.println(tree.root.right.isLeaf());
     }
 
 
