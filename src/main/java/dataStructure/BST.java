c4a98adaf420488da4ce8df0707dac5ca27f2411
Joao <pel118201@gmail.com>
2024-06-10 15:48:26 -0300

    Método insert

--- src/main/java/dataStructure/BST.java
+++ src/main/java/dataStructure/BST.java
@@ -1,18 +1,61 @@
 
 package dataStructure;
+
 // Binare Search Tree
 public class BST {
 
     public Node root;
 
-    public class Node{
+    public class Node {
         public int value;
         public Node left;
         public Node right;
 
         // Construtor
-        public Node(int value){
+        public Node(int value) {
             this.value = value;
         }
+    }
+    // Métodos
+
+    // Método de Inserir
+    public void insert(int value) {
+        if (root == null) {
+            root = new Node(value);
+        } else {
+            insert(root, value);
+        }
+    }
+
+    // Inserção de forma recursiva
+    private void insert(final Node root, final int value) {
+        if (root == null) return;
+        if (value == root.value) return;
+        if (value > root.value) {
+            if (root.right == null) root.right = new Node(value);
+            else insert(root.right, value);
+        } else {
+            if (root.left == null) root.left = new Node(value);
+            else insert(root.left, value);
+        }
+    }
+
+    // Método InOrder
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(final Tree.Node node) {
+        // E R D (Esquerda,raiz, direita)
+        if (node == null) return;
+        inOrder(node.left);
+        System.out.println(node.value);
+        inOrder(node.right);
+
+    }
+
+
+    public static void main(String[] args) {
+
     }
 }
