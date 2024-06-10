d7f4697479cb289316f7b1b5a241b40be6a7b38a
Joao <pel118201@gmail.com>
2024-06-10 16:43:50 -0300

    ReverseQueue

---
+++ src/main/java/problems/QueueReverse.java
@@ -0,0 +1,35 @@
+package problems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueReverse {
+
+    public static Queue<Integer> reverseFistK(Queue<Integer> queue, int k) {
+        solveQueue(queue, k);
+        int n = queue.size() - k;
+        while (n-- > 0) {
+            int element = queue.remove()
+            queue.add(element);
+        }
+
+    }
+
+    private static void solveQueue(Queue<Integer> queue, int k) {
+        if (k == 0) return;
+        int element = queue.remove();
+        solveQueue(queue, k - 1);
+        queue.add(element);
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        System.out.println(reverseFistK(queue, 3));
+    }
+}
