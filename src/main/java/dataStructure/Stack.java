5980f065608c02694b968d08616b0ae1f3ecf6f6
Joao <pel118201@gmail.com>
2024-06-10 12:15:54 -0300

    Método GetTop

--- src/main/java/dataStructure/Stack.java
+++ src/main/java/dataStructure/Stack.java
@@ -1,4 +1,35 @@
 package dataStructure;
 
 public class Stack {
+
+    private Node top;
+    private Node height;
+
+    class Node {
+        int value;
+        Node next;
+
+        // Construtor
+        Node(int value) {
+            this.value = value;
+        }
+
+    }
+
+
+    public Stack(int value) {
+        Node newNode = new Node(value);
+        top = newNode;
+        height = 1;
+    }
+    // Método GetTop
+    public void getTop(){
+        if (top == null){
+            System.out.println("Pilha Vazia");
+        } else{
+            System.out.println("Topo: " + top.value);
+        }
+    }
+
+
 }
