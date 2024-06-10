aed7d24fc03bdc079e3682e07ab644fa462f0950
Joao <pel118201@gmail.com>
2024-06-10 15:40:41 -0300

    Estrtura báscia de uma árvore binária de busca

---
+++ src/main/java/dataStructure/BST.java
@@ -0,0 +1,18 @@
+
+package dataStructure;
+// Binare Search Tree
+public class BST {
+
+    public Node root;
+
+    public class Node{
+        public int value;
+        public Node left;
+        public Node right;
+
+        // Construtor
+        public Node(int value){
+            this.value = value;
+        }
+    }
+}
