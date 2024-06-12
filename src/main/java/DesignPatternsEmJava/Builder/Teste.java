b1d4b7efc80eda3d6fa39c92c5dfcd47353a193f
Joao <pel118201@gmail.com>
2024-06-12 20:34:43 -0300

    PessoaBuilder

--- src/main/java/DesignPatternsEmJava/Builder/Teste.java
+++ src/main/java/DesignPatternsEmJava/Builder/Teste.java
@@ -5,14 +5,17 @@ import java.time.LocalDate;
 public class Teste {
 
     public static void main(String[] args) {
-        new PessoaBuilder()
+        Pessoa pessoa =
+                new PessoaBuilder()
                 .nome("João")
                 .sobreNome("Victor")
                 .documento("15715715757")
                 .email("zé@gmail.com")
                 .apelido("zé polvinho")
                 .dataNacimento(LocalDate.of(2003, 04, 02))
-                .criarPessoa()
+                .criarPessoa();
+
+        System.out.println(pessoa);
     }
 
 
