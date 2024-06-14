dbe9da89857212bc717ca993c5ed0ce0c7167b0f
Joao <pel118201@gmail.com>
2024-06-14 15:44:26 -0300

    Agenda

--- src/main/java/Singleton/TesteAgenda.java
+++ src/main/java/Singleton/TesteAgenda.java
@@ -7,11 +7,11 @@ public class TesteAgenda {
     public static void main(String[] args) {
 
         reservaDia("Sexta");
-       // reservaDia("Sábado");
+        reservaDia("Sábado");
 
     }
 
-    public static void reservaDia(String dia){
+    public static void reservaDia(String dia) {
         Agenda agenda = new Agenda();
         agenda.ocupa(dia);
         System.out.println(agenda.getDias());
