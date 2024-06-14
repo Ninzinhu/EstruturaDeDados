da83ffc8f990f7716acc54bd1014a2b4aa617df6
Joao <pel118201@gmail.com>
2024-06-14 16:35:04 -0300

    Agenda Singleton Lazy

--- src/main/java/Singleton/Solucao/TesteAgenda.java
+++ src/main/java/Singleton/Solucao/TesteAgenda.java
@@ -6,6 +6,20 @@ import java.util.Calendar;
 
 public class TesteAgenda {
     public static void main(String[] args) {
+//        AgendaSingletonEAGER agenda1 = AgendaSingletonEAGER.getInstance();
+//        AgendaSingletonEAGER agenda2 = AgendaSingletonEAGER.getInstance();
+//        System.out.println(agenda1);
+//        System.out.println(agenda2);
+//
+//
+//        reservaDiaEAGER("Sexta");
+//        reservaDiaEAGER("Sábado");
+
+
+        AgendaSingletonLAZY agenda1 = AgendaSingletonLAZY.getInstance();
+        AgendaSingletonLAZY agenda2 = AgendaSingletonLAZY.getInstance();
+        System.out.println(agenda1);
+        System.out.println(agenda2);
 
         reservaDiaEAGER("Sexta");
         reservaDiaEAGER("Sábado");
@@ -14,6 +28,14 @@ public class TesteAgenda {
 
     public static void reservaDiaEAGER(String dia) {
         AgendaSingletonEAGER agenda = AgendaSingletonEAGER.getInstance();
+        agenda.ocupa(dia);
+        System.out.println(agenda.getDias());
+
+        Calendar.getInstance();
+    }
+
+    public static void reservaDiaLAZY(String dia) {
+        AgendaSingletonLAZY agenda = AgendaSingletonLAZY.getInstance();
         agenda.ocupa(dia);
         System.out.println(agenda.getDias());
 
