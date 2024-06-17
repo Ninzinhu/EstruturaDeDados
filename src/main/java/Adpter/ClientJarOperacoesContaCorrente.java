ac9a975a2ae7118934ef6269f83d29245c3d015a
Joao <pel118201@gmail.com>
2024-06-17 19:20:30 -0300

    Adpter JarOperacoes

--- src/main/java/Adpter/ClientJarOperacoesContaCorrente.java
+++ src/main/java/Adpter/ClientJarOperacoesContaCorrente.java
@@ -1,4 +1,26 @@
 package Adpter;
 
+import java.math.BigDecimal;
+
 public class ClientJarOperacoesContaCorrente {
+
+    private JarOperacoesContaCorrente jocc;
+
+    public ClientJarOperacoesContaCorrente(JarOperacoesContaCorrente jocc) {
+        this.jocc = jocc;
+    }
+
+    public boolean validaSaldo(BigDecimal valorPretendido) {
+        return jocc.validaSaldo(valorPretendido);
+    }
+
+    public void saca(BigDecimal valorPretendido) {
+        jocc.saca(valorPretendido);
+    }
+
+    public void deposita(BigDecimal valor) {
+        jocc.deposita(valor);
+    }
+
+
 }
