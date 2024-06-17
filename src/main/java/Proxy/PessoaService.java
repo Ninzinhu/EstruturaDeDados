bdd282420a2b1f0ae031de773a98eac9eb890eda
Joao <pel118201@gmail.com>
2024-06-17 17:47:30 -0300

    Proxy Method part1

--- src/main/java/Proxy/PessoaService.java
+++ src/main/java/Proxy/PessoaService.java
@@ -1,4 +1,22 @@
 package Proxy;
 
+import DesignPatternsEmJava.Builder.Pessoa;
+
+
 public class PessoaService {
+
+    private PessoaRepository pessoaRepository;
+
+
+    public PessoaService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    public void save(Pessoa pessoa) {
+        pessoaRepository.save(pessoa);
+    }
+
+    public Pessoa findById(Long id) {
+        return pessoaRepository.findById(id);
+    }
 }
