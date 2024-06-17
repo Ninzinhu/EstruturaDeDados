21b294045e6944d21c609c5f64677a6e58a4be1d
Joao <pel118201@gmail.com>
2024-06-17 19:35:46 -0300

    Adpter TesteOperacoesContaCorrenteAdpter

--- src/main/java/Adpter/Solução/TesteOperacoesContaCorrenteAdpter.java
+++ src/main/java/Adpter/Solução/TesteOperacoesContaCorrenteAdpter.java
@@ -1,4 +1,23 @@
 package Adpter.Solução;
 
+import Adpter.ClientJarOperacoesContaCorrente;
+import Adpter.JarOperacoesContaCorrente;
+
+import java.math.BigDecimal;
+
 public class TesteOperacoesContaCorrenteAdpter {
+
+    public static void main(String[] args) {
+
+
+        JarOperacoesContaCorrente jarConta = new JarOperacoesContaCorrente();
+        ClientJarOperacoesContaCorrente client = new ClientJarOperacoesContaCorrente(jarConta);
+
+        BigDecimal valorPretendidoParaSaque = new BigDecimal(1000);
+
+        if (client.validaSaldo(valorPretendidoParaSaque)){
+            client.saca(valorPretendidoParaSaque);
+        }
+        client.deposita(BigDecimal.valueOf(500));
+    }
 }
