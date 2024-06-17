133c4d546b4e1d28a44502fea6c85afd684a1c6e
Joao <pel118201@gmail.com>
2024-06-17 18:45:38 -0300

    Adicionado Cache

--- src/main/java/Proxy/Resolução/PessoaRepositoryProxy.java
+++ src/main/java/Proxy/Resolução/PessoaRepositoryProxy.java
@@ -3,12 +3,17 @@ package Proxy.Resolução;
 import DesignPatternsEmJava.Builder.Pessoa;
 import Proxy.PessoaRepository;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.logging.Logger;
 
 public class PessoaRepositoryProxy extends PessoaRepository {
 
 
     private static Logger log = Logger.getLogger(PessoaRepositoryProxy.class.getName());
+
+    private Map<Long, Pessoa> cache = new HashMap<>();
 
     @Override
     public void save(Pessoa pessoa) {
@@ -20,8 +25,26 @@ public class PessoaRepositoryProxy extends PessoaRepository {
     @Override
     public Pessoa findById(Long id) {
         log.info("Iniciando chamada do metodo FindById.....");
-        Pessoa pessoa = super.findById(id);
+        long inicio = System.currentTimeMillis();
+
+        Pessoa pessoa = null;
+
+        if (Objects.nonNull(cache.get(id))) {
+            log.info("Cache inicializado.....");
+            pessoa = cache.get(id);
+
+        } else {
+            log.info("Esta pegando a classe do PessoaRepository.....");
+             pessoa = super.findById(id);
+            cache.put(id, pessoa);
+        }
+
+
         log.info("Finalizando chamada do metodo FindById.....");
+        long fim = System.currentTimeMillis();
+
+        log.info("Tempo gasto na chamada....." + (fim - inicio));
         return pessoa;
     }
+
 }
