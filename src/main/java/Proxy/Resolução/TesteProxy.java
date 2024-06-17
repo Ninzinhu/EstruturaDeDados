e86c17f4e56f304fe82a8db40f0b4a576c46c79a
Joao <pel118201@gmail.com>
2024-06-17 18:25:58 -0300

    Proxy Method

--- src/main/java/Proxy/Resolução/TesteProxy.java
+++ src/main/java/Proxy/Resolução/TesteProxy.java
@@ -1,8 +1,29 @@
 package Proxy.Resolução;
 
+import DesignPatternsEmJava.Builder.Pessoa;
+import DesignPatternsEmJava.Builder.PessoaBuilder;
+import Proxy.PessoaRepository;
+import Proxy.PessoaService;
+
+import java.time.LocalDate;
+
 public class TesteProxy {
 
     public static void main(String[] args) {
-        
+        PessoaService pessoaService = new PessoaService(new PessoaRepository());
+
+        Pessoa pessoa = new PessoaBuilder()
+                .nome("João")
+                .sobreNome("Victor")
+                .documento("123456789")
+                .email("joao.victor@example.com")
+                .apelido("Jo")
+                .dataNascimento(LocalDate.of(2003, 4, 2))
+                .criarPessoa();
+
+        pessoaService.save(pessoa);
+
+        Pessoa pessoaRetornada = pessoaService.findById(1L);
+        System.out.println(pessoaRetornada);
     }
 }
