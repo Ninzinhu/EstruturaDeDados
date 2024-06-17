81d68949bb361462399e054ef9206277ef08c19a
Joao <pel118201@gmail.com>
2024-06-17 20:58:26 -0300

    STRATEGY Funcionario

---
+++ src/main/java/Strategy/Funcionario.java
@@ -0,0 +1,36 @@
+package Strategy;
+
+import java.math.BigDecimal;
+
+public class Funcionario {
+
+    private String nome;
+
+    private BigDecimal salario;
+
+    private TipoContratacaoEnum tipoContratacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public BigDecimal getSalario() {
+        return salario;
+    }
+
+    public void setSalario(BigDecimal salario) {
+        this.salario = salario;
+    }
+
+    public TipoContratacaoEnum getTipoContratacao() {
+        return tipoContratacao;
+    }
+
+    public void setTipoContratacao(TipoContratacaoEnum tipoContratacao) {
+        this.tipoContratacao = tipoContratacao;
+    }
+}
