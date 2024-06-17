c99b51210bb4a2a422281a189f5236eecf2bdf5d
Joao <pel118201@gmail.com>
2024-06-17 18:45:30 -0300

    Resolução Proxy

--- src/main/java/Proxy/TesteProxy.java
+++ src/main/java/Proxy/TesteProxy.java
@@ -8,21 +8,21 @@ import java.time.LocalDate;
 public class TesteProxy {
 
     public static void main(String[] args) {
-        PessoaService pessoaService = new PessoaService(new PessoaRepository());
-
-        Pessoa pessoa = new PessoaBuilder()
-                .nome("João")
-                .sobreNome("Victor")
-                .documento("123456789")
-                .email("joao.victor@example.com")
-                .apelido("Jo")
-                .dataNascimento(LocalDate.of(2003, 4, 2))
-                .criarPessoa();
-
-        pessoaService.save(pessoa);
-
-        Pessoa pessoaRetornada = pessoaService.findById(1L);
-        System.out.println(pessoaRetornada);
+//        PessoaService pessoaService = new PessoaService(new PessoaRepository());
+//
+//        Pessoa pessoa = new PessoaBuilder()
+//                .nome("João")
+//                .sobreNome("Victor")
+//                .documento("123456789")
+//                .email("joao.victor@example.com")
+//                .apelido("Jo")
+//                .dataNascimento(LocalDate.of(2003, 4, 2))
+//                .criarPessoa();
+//
+//        pessoaService.save(pessoa);
+//
+//        Pessoa pessoaRetornada = pessoaService.findById(1L);
+//        System.out.println(pessoaRetornada);
 
 //        System.out.println("Nome: " + pessoa.getNome());
 //        System.out.println("Sobrenome: " + pessoa.getSobreNome());
