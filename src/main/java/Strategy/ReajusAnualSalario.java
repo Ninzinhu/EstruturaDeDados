365e0383eb7ec250d2b0722e550a0d7c02cfa820
Joao <pel118201@gmail.com>
2024-06-17 21:20:01 -0300

    Strategy

--- src/main/java/Strategy/ReajusAnualSalario.java
+++ src/main/java/Strategy/ReajusAnualSalario.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 
 public class ReajusAnualSalario {
 
-    public void calculeReajusteAnual(Funcionario funcionario){
-        if(TipoContratacaoEnum.CLT.name().equals(funcionario.getTipoContratacao().name())){
+    public void calculeReajusteAnual(Funcionario funcionario) {
+        if (TipoContratacaoEnum.CLT.name().equals(funcionario.getTipoContratacao().name())) {
             BigDecimal salario = funcionario.getSalario();
             funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.10))));
-        } else if (TipoContratacaoEnum.PJ.equals(funcionario.getTipoContratacao())){
+        } else if (TipoContratacaoEnum.PJ.equals(funcionario.getTipoContratacao())) {
             BigDecimal salario = funcionario.getSalario();
             funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.05))));
-        } else if (TipoContratacaoEnum.ESTAGIO.equals(funcionario.getTipoContratacao())){
+        } else if (TipoContratacaoEnum.ESTAGIO.equals(funcionario.getTipoContratacao())) {
             BigDecimal salario = funcionario.getSalario();
             funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.02))));
 
