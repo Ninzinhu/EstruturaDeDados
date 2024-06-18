c5f5814d8e6427f451e71eaf84b75f918bb150a4
Joao <pel118201@gmail.com>
2024-06-17 22:54:11 -0300

    Reajuste

--- src/main/java/Strategy/ReajusAnualSalario.java
+++ src/main/java/Strategy/ReajusAnualSalario.java
@@ -13,17 +13,7 @@ public class ReajusAnualSalario {
     private void calculadorReajusteAnunalSalario(Funcionario funcionario) {
     }
 
-    if (TipoContratacaoEnum.CLT.name().equals(funcionario.getTipoContratacao().name())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.10))));
-    } else if (TipoContratacaoEnum.PJ.equals(funcionario.getTipoContratacao())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.05))));
-    } else if (TipoContratacaoEnum.ESTAGIO.equals(funcionario.getTipoContratacao())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.02))));
 
-    }
 
 
 }
--- src/main/java/Strategy/Solucao/ReajusteSalarioComStrategy.java
+++ src/main/java/Strategy/Solucao/ReajusteSalarioComStrategy.java
@@ -14,15 +14,5 @@ public class ReajusteSalarioComStrategy {
     private void calculadorReajusteAnunalSalario(Funcionario funcionario) {
     }
 
-     if (TipoContratacaoEnum.CLT.name().equals(funcionario.getTipoContratacao().name())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.10))));
-    } else if (TipoContratacaoEnum.PJ.equals(funcionario.getTipoContratacao())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.05))));
-    } else if (TipoContratacaoEnum.ESTAGIO.equals(funcionario.getTipoContratacao())) {
-        BigDecimal salario = funcionario.getSalario();
-        funcionario.setSalario(salario.add(salario.multiply(new BigDecimal(0.02))));
 
-    }
 }
