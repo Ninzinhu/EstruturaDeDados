4eeb976f5f312ab1014feb3332e42735bd6c42c2
Joao <pel118201@gmail.com>
2024-06-17 23:32:29 -0300

    RepararveiculosComun Método

--- src/main/java/Template/Problema/RepararVeiculoComumService.java
+++ src/main/java/Template/Problema/RepararVeiculoComumService.java
@@ -1,4 +1,48 @@
 package Template.Problema;
 
+import Template.VeiculoParaReparo;
+
 public class RepararVeiculoComumService {
+
+    private VeiculoParaReparo veiculoParaReparo;
+
+    public RepararVeiculoComumService(VeiculoParaReparo veiculoParaReparo) {
+        this.veiculoParaReparo = veiculoParaReparo;
+    }
+
+    public void reparaVeiculo(){
+        entradaOficina();
+        analisarDanos();
+        if(veiculoParaReparo()){
+            repararVeiculo();
+            notificarReparoParaSeguradora();
+        } else {
+            notificarPerdaTotalParaSeguradora();
+        }
+    }
+
+    private void notificarPerdaTotalParaSeguradora() {
+        System.out.println("Notificando perda total para Seguradora");
+    }
+
+    private void notificarReparoParaSeguradora() {
+        System.out.println("Notificando reparo para Seguradora");
+
+    }
+
+    private void repararVeiculo() {
+        System.out.println("Reparando Veiculo...");
+
+    }
+
+    private void analisarDanos() {
+        System.out.println("Analisando Danos...");
+
+    }
+
+    private void entradaOficina() {
+        System.out.println("Veiculo entrando Oficina...");
+
+
+    }
 }
