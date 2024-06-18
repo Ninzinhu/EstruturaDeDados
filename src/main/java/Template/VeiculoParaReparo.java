bdf6d5798bd8919389dc25edfb75811c28394569
Joao <pel118201@gmail.com>
2024-06-17 23:25:24 -0300

    VeiculoParaReparo

--- src/main/java/Template/VeiculoParaReparo.java
+++ src/main/java/Template/VeiculoParaReparo.java
@@ -1,4 +1,14 @@
 package Template;
 
 public class VeiculoParaReparo {
+
+    private int porcentagemDano;
+
+    public int getPorcentagemDano() {
+        return porcentagemDano;
+    }
+
+    public void setPorcentagemDano(int porcentagemDano) {
+        this.porcentagemDano = porcentagemDano;
+    }
 }
