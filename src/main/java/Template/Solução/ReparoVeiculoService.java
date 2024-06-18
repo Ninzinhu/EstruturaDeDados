41f3499027c8b6e9d3064f5dc8cdc242e9e4a0f8
Joao <pel118201@gmail.com>
2024-06-17 23:50:49 -0300

    ReparoVeiculoService

--- src/main/java/Template/Solução/ReparoVeiculoService.java
+++ src/main/java/Template/Solução/ReparoVeiculoService.java
@@ -1,4 +1,42 @@
 package Template.Solução;
 
-public class ReparoVeiculoService {
+public abstract class ReparoVeiculoService {
+
+    public void reparaVeiculo() {
+        entradaOficina();
+        analisarDanos();
+        if (veiculoParaReparo()) {
+            repararVeiculo();
+            notificarReparoParaSeguradora();
+        } else {
+            notificarPerdaTotalParaSeguradora();
+        }
+    }
+
+    protected abstract boolean veiculoParaReparo();
+
+    private void notificarPerdaTotalParaSeguradora() {
+        System.out.println("Notificando perda total para Seguradora..");
+    }
+
+    private void notificarReparoParaSeguradora() {
+        System.out.println("Notificando reparo para Seguradora...");
+
+    }
+
+    private void repararVeiculo() {
+        System.out.println("Reparando Veiculo...");
+
+    }
+
+    private void analisarDanos() {
+        System.out.println("Analisando Danos...");
+
+    }
+
+    private void entradaOficina() {
+        System.out.println("Veiculo entrando Oficina...");
+
+
+    }
 }
