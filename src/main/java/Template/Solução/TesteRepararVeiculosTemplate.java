aa57a02ba170f5f9c6690fbe457142d56f108802
Joao <pel118201@gmail.com>
2024-06-17 23:55:07 -0300

    Reparo Solução

---
+++ src/main/java/Template/Solução/TesteRepararVeiculosTemplate.java
@@ -0,0 +1,28 @@
+package Template.Solução;
+
+import Template.Problema.RepararVeiculoComumService;
+import Template.Problema.RepararVeiculoDeLuxoService;
+import Template.VeiculoParaReparo;
+
+public class TesteRepararVeiculosTemplate {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------LUXO---------------------");
+
+        VeiculoParaReparo veiculoDeLuxo = new VeiculoParaReparo();
+        veiculoDeLuxo.setPorcentagemDano(51);
+
+        RepararVeiculoDeLuxoService rpl = new RepararVeiculoDeLuxoService(veiculoDeLuxo);
+
+        rpl.reparaVeiculo();
+
+        System.out.println("------------------COMUM---------------------");
+
+        VeiculoParaReparo veiculoComum = new VeiculoParaReparo();
+        veiculoComum.setPorcentagemDano(59);
+
+        RepararVeiculoComumService rpc = new RepararVeiculoComumService(veiculoComum);
+
+        rpc.reparaVeiculo();
+    }
+}
