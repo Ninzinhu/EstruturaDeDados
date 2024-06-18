a3fc2a41000dd10df421ad4d2c0ca45d62809554
Joao <pel118201@gmail.com>
2024-06-17 22:21:32 -0300

    Strategy

--- src/main/java/Strategy/TesteAjusteSalario.java
+++ src/main/java/Strategy/TesteAjusteSalario.java
@@ -1,8 +1,30 @@
 package Strategy;
 
+import java.math.BigDecimal;
+
 public class TesteAjusteSalario {
 
     public static void main(String[] args) {
+
+        ReajusAnualSalario reajusAnualSalario = new ReajusAnualSalario();
+        Funcionario funcionarioCLT = new Funcionario();
+        funcionarioCLT.setNome("Victor");
+        funcionarioCLT.setTipoContratacao(TipoContratacaoEnum.CLT);
+        funcionarioCLT.setSalario(new BigDecimal(5000));
+
+        Funcionario funcionarioPJ = new Funcionario();
+        funcionarioPJ.setNome("Podolski");
+        funcionarioPJ.setTipoContratacao(TipoContratacaoEnum.PJ);
+        funcionarioPJ.setSalario(new BigDecimal(10000));
+
+        Funcionario funcionarioEstagio = new Funcionario();
+        funcionarioEstagio.setNome("Osvaldo");
+        funcionarioEstagio.setTipoContratacao(TipoContratacaoEnum.ESTAGIO);
+        funcionarioEstagio.setSalario(new BigDecimal(2000));
+
+        // Clacular nossos reajustes aqui
+
+
 
     }
 }
